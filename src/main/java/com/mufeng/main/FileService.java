4673785bb03559e07658f9053ba500cc65330f3a
jiazaibo <zbjia@isoftstone.com>
2015-09-29 11:03:12 +0800

    modify

--- src/main/java/com/mufeng/main/FileService.java
+++ src/main/java/com/mufeng/main/FileService.java
@@ -1,17 +1,29 @@
 package com.mufeng.main;
 
 public class FileService {
-	
+	/**
+	 * 上传
+	 * @param fileName
+	 * @return
+	 */
 	public String upload(String fileName) {
 		System.out.println(fileName);
 		return fileName;
 	}
-	
+	/**
+	 * 下载
+	 * @param fileName
+	 * @return
+	 */
 	public byte[] download(String fileName) {
 		return "mufeng".getBytes();
 	}
 	
-	
+	/**
+	 * 获取状态
+	 * @param fileName
+	 * @return
+	 */
 	public int getState(String fileName) {
 		
 		if ("mufeng.txt".equals(fileName)) {
